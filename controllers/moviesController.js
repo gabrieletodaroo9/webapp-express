53ecaedf211414f7e91479621ef7e3a1919c17b6
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-12-10 15:46:11 +0100

    create and test show route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -7,10 +7,30 @@ const index = (req, res) => {
         res.json(results)
     })
 }
+
 const show = (req, res) => {
-    res.send('Movie with id:?')
+
+    const id = req.params.id
+    const moviesSql = 'SELECT * FROM movies WHERE id = ?'
+    const reviewSql = `SELECT id,text AS 'review',vote FROM reviews WHERE movie_id = ?`
+
+    connection.query(moviesSql, [id], (err, moviesResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (moviesResults.length === 0) return res.status(404).json({ error: 'Movie not found' })
+
+        const movie = moviesResults[0]
+
+        connection.query(reviewSql, [id], (err, reviewResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' })
+
+            movie.reviews = reviewResults
+            res.json(movie)
+        })
+    })
 }
 
+
 module.exports = {
-    index
+    index,
+    show
 }
